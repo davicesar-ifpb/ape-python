3c422e8e11ab53248a4052d2c8b9aef14f473ee9
Davi César <160984901+davicesar-ifpb@users.noreply.github.com>
2024-03-12 17:08:57 -0300

    12/03/2024

--- Listas/Lista decisao/questao_1.py
+++ Listas/Lista decisao/questao_1.py
@@ -1,4 +1,4 @@
-numero = float(input("Digite um número: "))
+numero = int(input("Digite um número inteiro: "))
 if numero % 2 == 0:
     print("Par")
 else:
--- Listas/Lista decisao/questao_3.py
+++ Listas/Lista decisao/questao_3.py
@@ -2,9 +2,18 @@ num1 = float(input("Digite o número 1: "))
 num2 = float(input("Digite o número 2: "))
 num3 = float(input("Digite o número 3: "))
 
+
 if num1 > num2 and num1 > num3:
     print(num1)
-elif num2 > num1 and num2 > num3:
+elif num2 > num3:
     print(num2)
 else:
     print(num3)
+
+
+lista = []
+for i in range(1,4):
+    num = float(input(f"Digite o número {i}: "))
+    lista.append(num)
+lista.sort()
+print(lista[-1])
