896ef2e81bcce80499459f4450d2f8e82ca8b8bd
Davi César <davicesarmorais@gmail.com>
2024-03-16 16:33:27 -0300

    .

---
+++ Area de testes/ASDAD.py
@@ -0,0 +1,3 @@
+lista = [4,124,5,1,32,1]
+
+print("".join((str(lista[0: (len(lista))])).split(",")).strip("[]"))
--- Outros estudos/Estiagem (1023).py
+++ Outros estudos/Estiagem (1023).py
@@ -5,35 +5,34 @@ while True:
         break
     
     lista = []
-    i = imoveis
-    while i > 0:
-        pessoa,consumo = [int(x) for x in input().split()]
-        lista.append((pessoa,consumo))
-        i -= 1
-    
-    consumos = []
-    consumos_ordenar = []
-    pessoas = []
     soma_consumos = 0
     soma_pessoas = 0
-    for p,c in lista:
-        soma_consumos = soma_consumos + c
-        soma_pessoas = soma_pessoas + p
-        consumo_por_pessoa = c // p
-        if consumo_por_pessoa in consumos:      
-            sominha = pessoas[consumos.index(consumo_por_pessoa)] + p
-            pessoas[consumos.index(consumo_por_pessoa)] = sominha
+    i = imoveis
+    z = -1
+    while i > 0:
+        pessoa,consumo = map(int, input().split())
+        soma_consumos = soma_consumos + consumo
+        soma_pessoas = soma_pessoas + pessoa
+        consumo_por_pessoa = consumo // pessoa
+        lista.append((consumo_por_pessoa,pessoa))
+        if z >= 0:
+            if consumo_por_pessoa == lista[z][0]:
+                consumo_novo = lista[z][0]
+                pessoa_novo = lista[z][1] + pessoa
+                lista.pop()
+                lista.pop()
+                lista.append((consumo_novo,pessoa_novo))
+            else:
+                z += 1
         else:
-            consumos.append(consumo_por_pessoa)
-            consumos_ordenar.append(consumo_por_pessoa)
-            pessoas.append(p)
+            z += 1
+        i -= 1
     
-    consumos_ordenar.sort()
-    print(f"Cidade# {cidade}:")
-    for c in consumos_ordenar:
-        index = consumos.index(c)
-        print(f"{pessoas[index]}-{c}",end=" ")
+    lista.sort()
     consumo_medio = soma_consumos / soma_pessoas
     consumo_medio_truncado = int(consumo_medio * 100) / 100
+    print(f"Cidade# {cidade}:")
+    for c,p in lista:
+        print(f"{p}-{c}",end=" ")
     print(f"\nConsumo medio: {(consumo_medio_truncado):.2f} m3.")
     cidade += 1
