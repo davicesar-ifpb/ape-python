0000bc9aa489ebe060e2e394cab83dec770e6e6a
dcZIN <davicesarmorais@gmail.com>
2024-03-18 12:35:41 -0300

    a

--- Area de testes/ASDAD.py
+++ Area de testes/ASDAD.py
@@ -1,3 +1,10 @@
 lista = [4,124,5,1,32,1]
 
-print("".join((str(lista[0: (len(lista))])).split(",")).strip("[]"))
+print(4,124,5,1,32,1)
+
+for  i in lista:
+    print(i,end=" ")
+print()
+
+print("".join((str(lista)).split(",")).strip("[]"))
+
