656f246ca59212092aa4e35d45d69ee4b83d3062
Davi César <davi.cesar@academico.ifpb.edu.br>
2024-08-06 17:43:16 -0300

    upd

--- Listas/Lista_vetor/q_4.py
+++ Listas/Lista_vetor/q_4.py
@@ -2,8 +2,8 @@ numeros = [int(input(f"Vetor[{i}] = ")) for i in range(10)]
 
 print("\nNumeros de INDICES pares:")
 for i in range(0, 10, 2):
-    print(f"numeros{i} =", numeros[i])
+    print(f"numeros[{i}] =", numeros[i])
 
 print("\nNumeros de INDICES impares:")
 for i in range(1, 10, 2):
-    print(f"numeros{i} =", numeros[i])
+    print(f"numeros[{i}] =", numeros[i])
--- Listas/Lista_vetor/q_6.py
+++ Listas/Lista_vetor/q_6.py
@@ -4,6 +4,11 @@ vetor = [int(input(f"Vetor[{i}] = ")) for i in range(n)]
 
 print("\nVetor antes:", vetor)
 
-vetor = vetor[::-1]
+#vetor = vetor[::-1]
+#vetor = [vetor[i] for i in range(n - 1, -1, -1)]
+#vetor = reveserd(vetor)
+
+for i in range(n // 2):
+    vetor[i], vetor[n-1-i] = vetor[n-1-i], vetor[i]
 
 print("Vetor depois:",vetor)
